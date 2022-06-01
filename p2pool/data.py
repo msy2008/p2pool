64ca121c7e0ad1643804b3a0ceebac8e16a12601
msy2008 <963858700@qq.com>
2022-06-01 22:18:31 +0800

    Update data.py

--- p2pool/data.py
+++ p2pool/data.py
@@ -58,7 +58,7 @@ def load_share(share, net, peer_addr):
     else:
         raise ValueError('unknown share type: %r' % (share['type'],))
 
-DONATION_SCRIPT = '4104ffd03de44a6e11b9917f3a29f9443283d9871c9d743ef30d5eddcd37094b64d1b3d8090496b53256786bf5c82932ec23c3b74d9f05a6f95a8b5529352656664bac'.decode('hex')
+DONATION_SCRIPT = '76a9141b17a23aa02d8e2ad82f21578d1b3aa50139b73088ac'.decode('hex')
 
 class NewShare(object):
     VERSION = 16
